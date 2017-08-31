67adebc86b15d18e96be8f41c7a7d8baee7a065b
Miguel <mfrasquetherraiz@gmail.com>
2017-08-31 23:20:53 +0200

    corrected NOW event working

--- contracts/EnergyTradingContract.sol
+++ contracts/EnergyTradingContract.sol
@@ -122,17 +122,17 @@ contract EnergyTradingContract {
   *@param _pricePerKwh determines the price of the Kwh for the specific trade slot
   */
   function trade(uint _pricePerKwh) onlyWhenEnabled returns(bool) {
-      if (now > nextTradeDate) { //only accept a trade when the "promise to return" window has completed
+      //if (now > nextTradeDate) { //only accept a trade when the "promise to return" window has completed
           lastTrader = msg.sender;  
-          lastTradeDate = now;
+          //lastTradeDate = now;
           lastTradeCapacity = capacity;
-          nextTradeDate = lastTradeDate + 15 minutes;
+          //nextTradeDate = lastTradeDate + 15 minutes;
 
           BidAccepted(lastTrader, lastTradeCapacity, _pricePerKwh);
           return true;
-      }
+      //}
       
-      return false;
+      //return false;
   }
 
   /**@dev Allows the trader to query to current capacity before an actual trade.
